cbf7cfaf943fcb2a7725654c9feb4784e3fc1c66
jelle van den toren <jellevdtoren@hotmail.com>
2017-03-01 13:32:26 +0100

    Add files via upload

--- +QRS/Analyzer.m
+++ +QRS/Analyzer.m
@@ -1,88 +1,88 @@
 classdef Analyzer < handle
-  
-  properties
-    AVG;
-  end
-  
-  methods
-    function self = Analyzer(channels)
-      % get avarage of all signals
-      % This should be private, but public now for testing purposes
-      self.AVG = zeros(9999, 1);
-      
-      channels(:, 1) = NaN;
-      channels(:, 8) = NaN;
-      channels(:, 184) = NaN;
-      channels(:, 192) = NaN;
-      
-      for I = 1:size(channels(:, 1))
-        self.AVG(I) = mean(channels(I, :), 'omitnan');
-      end
-      
-      self.AVG = sgolayfilt(self.AVG, 3, 101);
+    
+    properties
+        AVG;
     end
     
-    function ret = qrs(self)
-      r = self.getR;
-      qs = self.getQS;
-      ret = self.getQRS(r, qs);
-    end
-    
-    function ret = manualQRS(self)
-        ret = self.getManualQRS();
-    end
-    
-  end
-  
-  methods (Access = private)
-    % vanaf hier moet ik gaan beginnen
-    function ret = getR(self)
-      % get R wave from gradient
-      % private
-      ecg = sgolayfilt(gradient(self.AVG), 3, 51);
-      
-      [~, ret] = findpeaks(ecg, 'MinPeakDistance', 200, ...
-        'MinPeakHeight', 0.03); % find R peaks 300 samples appart
-    end
-    % mogelijk heeft dit ook nog verbetering nodig
-    function ret = getQS(self)
-      % get QS wave from gradient
-      % private
-      ecg = gradient(sign(gradient(self.AVG)));
-      
-      ret = find(ecg == -1); % detect Q and S peaks
-    end
-    
-    function ret = getQRS(~, locs_R, locs_QS)
-      % returns begin and end of QRS complex
-      qrs = zeros(length(locs_R),2); % pre allocate matrix
-      
-      for I = 1: length(locs_R)
-        locs_plus = locs_QS - locs_R(I);
-        locs_min = -locs_plus;
+    methods
+        function self = Analyzer(channels)
+            % get avarage of all signals
+            % This should be private, but public now for testing purposes
+            self.AVG = zeros(9999, 1);
+            
+            channels(:, 1) = NaN;
+            channels(:, 8) = NaN;
+            channels(:, 184) = NaN;
+            channels(:, 192) = NaN;
+            
+            for I = 1:size(channels(:, 1))
+                self.AVG(I) = mean(channels(I, :), 'omitnan');
+            end
+            
+            self.AVG = sgolayfilt(self.AVG, 3, 101);
+        end
         
-        locs_plus(locs_plus<0) = Inf;
-        locs_min(locs_min<0) = Inf;
+        function ret = qrs(self)
+            r = self.getR;
+            qs = self.getQS;
+            ret = self.getQRS(r, qs);
+        end
         
-        [~,xQ] = min(locs_min);
-        [~,xS] = min(locs_plus);
-        
-        % store Q and S locations in qrs
-        qrs(I,1:2)= [locs_QS(xQ) locs_QS(xS)];
-      end
-      
-      ret = qrs;
-    end
-    %%---------------------------------------------------------
-    % code jelle, function for analysing the manual qrs markings
-    function dataQrs = getManualQRS(location_QRS)
-        Qrs = location_QRS;
-        
-        dataQrs = Qrs;
+        function ret = manualQRS(self)
+            ret = self.getManualQRS();
+        end
         
     end
     
-    %%---------------------------------------------------------
-  end % end methods
-  
+    methods (Access = private)
+        % vanaf hier moet ik gaan beginnen
+        function ret = getR(self)
+            % get R wave from gradient
+            % private
+            ecg = sgolayfilt(gradient(self.AVG), 3, 51);
+            
+            [~, ret] = findpeaks(ecg, 'MinPeakDistance', 200, ...
+                'MinPeakHeight', 0.03); % find R peaks 300 samples appart
+        end
+        % mogelijk heeft dit ook nog verbetering nodig
+        function ret = getQS(self)
+            % get QS wave from gradient
+            % private
+            ecg = gradient(sign(gradient(self.AVG)));
+            
+            ret = find(ecg == -1); % detect Q and S peaks
+        end
+        
+        function ret = getQRS(~, locs_R, locs_QS)
+            % returns begin and end of QRS complex
+            qrs = zeros(length(locs_R),2); % pre allocate matrix
+            
+            for I = 1: length(locs_R)
+                locs_plus = locs_QS - locs_R(I);
+                locs_min = -locs_plus;
+                
+                locs_plus(locs_plus<0) = Inf;
+                locs_min(locs_min<0) = Inf;
+                
+                [~,xQ] = min(locs_min);
+                [~,xS] = min(locs_plus);
+                
+                % store Q and S locations in qrs
+                qrs(I,1:2)= [locs_QS(xQ) locs_QS(xS)];
+            end
+            
+            ret = qrs;
+        end
+        %%---------------------------------------------------------
+        % code jelle, function for analysing the manual qrs markings
+        function dataQrs = getManualQRS(location_QRS)
+            Qrs = location_QRS;
+            
+            dataQrs = Qrs;
+            
+        end
+        
+        %%---------------------------------------------------------
+    end % end methods
+    
 end % end classdef
--- +UI/Selector.m
+++ +UI/Selector.m
@@ -1,187 +1,187 @@
 classdef Selector < handle
-  % 8 by N matrix of pixel to choose channel
-  properties
-    % some settings
-    view;
-    mat;
-    val;
-    selected = 1;
-    xv;
-    yv;
-    xh;
-    yh;
-    hStrings;
-    callback;
-  end
-  
-  methods
-    function self = Selector(view)
-      self.mat = zeros(192/8, 8);
-      self.mat(1) = 1;
-      self.val = self.mat;
-      self.view = view;
-      
-      for I = 1:192/8
-        for J = 1:8
-          self.val(I,J) = J + (I-1) * 8;
+    % 8 by N matrix of pixel to choose channel
+    properties
+        % some settings
+        view;
+        mat;
+        val;
+        selected = 1;
+        xv;
+        yv;
+        xh;
+        yh;
+        hStrings;
+        callback;
+    end
+    
+    methods
+        function self = Selector(view)
+            self.mat = zeros(192/8, 8);
+            self.mat(1) = 1;
+            self.val = self.mat;
+            self.view = view;
+            
+            for I = 1:192/8
+                for J = 1:8
+                    self.val(I,J) = J + (I-1) * 8;
+                end
+            end
+            
+            pan(self.view, 'off');
         end
-      end
-      
-      pan(self.view, 'off');
-    end
-    
-    function setCallback(self, x)
-      self.callback = x;
-    end
-    
-    function show(self)
-      % display the selectors
-      colormap(self.view, [1,1,1;1,0,0]);
-      imagesc(self.view, self.mat);
-      
-      self.createGrid;
-      self.displayGrid;
-      self.displayText;
-      set(self.view,'XTick',[], 'XTickLabel',[],...
-        'YTick',[], 'YTickLabel',[], 'TickLength', [0 0]);
-    end
-    
-    function next(self)
-      % move to next channel
-      if self.selected ~= 192
-        [column, row] = self.selection(self.selected);
-        self.mat(row, column) = 0;
-        self.selected = self.selected + 1;
-        [column, row] = self.selection(self.selected);
-        self.mat(row, column) = 1;
         
-        self.update;
+        function setCallback(self, x)
+            self.callback = x;
+        end
         
-        self.callback(self.selected);
-      end
-    end
-    
-    function previous(self)
-      % move to previous channel
-      if self.selected ~= 1
-        [column, row] = self.selection(self.selected);
-        self.mat(row, column) = 0;
-        self.selected = self.selected - 1;
-        [column, row] = self.selection(self.selected);
-        self.mat(row, column) = 1;
+        function show(self)
+            % display the selectors
+            colormap(self.view, [1,1,1;1,0,0]);
+            imagesc(self.view, self.mat);
+            
+            self.createGrid;
+            self.displayGrid;
+            self.displayText;
+            set(self.view,'XTick',[], 'XTickLabel',[],...
+                'YTick',[], 'YTickLabel',[], 'TickLength', [0 0]);
+        end
         
-        self.update;
+        function next(self)
+            % move to next channel
+            if self.selected ~= 192
+                [column, row] = self.selection(self.selected);
+                self.mat(row, column) = 0;
+                self.selected = self.selected + 1;
+                [column, row] = self.selection(self.selected);
+                self.mat(row, column) = 1;
+                
+                self.update;
+                
+                self.callback(self.selected);
+            end
+        end
         
-        self.callback(self.selected);
-      end
+        function previous(self)
+            % move to previous channel
+            if self.selected ~= 1
+                [column, row] = self.selection(self.selected);
+                self.mat(row, column) = 0;
+                self.selected = self.selected - 1;
+                [column, row] = self.selection(self.selected);
+                self.mat(row, column) = 1;
+                
+                self.update;
+                
+                self.callback(self.selected);
+            end
+        end
     end
-  end
-  
-  methods (Access = private)
-    % private function/methods
-    function createGrid(self)
-      % create grid
-      h = findobj(gcf,'type','image');
-      
-      xdata = get(h, 'XData');
-      ydata = get(h, 'YData');
-      
-      M = size(get(h,'CData'), 1);
-      N = size(get(h,'CData'), 2);
-      
-      if M > 1
-        pixel_height = diff(ydata) / (M-1);
+    
+    methods (Access = private)
+        % private function/methods
+        function createGrid(self)
+            % create grid
+            h = findobj(gcf,'type','image');
+            
+            xdata = get(h, 'XData');
+            ydata = get(h, 'YData');
+            
+            M = size(get(h,'CData'), 1);
+            N = size(get(h,'CData'), 2);
+            
+            if M > 1
+                pixel_height = diff(ydata) / (M-1);
+                
+            else
+                pixel_height = 1;
+            end
+            
+            if N > 1
+                pixel_width = diff(xdata) / (N-1);
+            else
+                pixel_width = 1;
+            end
+            
+            y_top = ydata(1) - (pixel_height/2);
+            y_bottom = ydata(2) + (pixel_height/2);
+            y = linspace(y_top, y_bottom, M+1);
+            
+            x_left = xdata(1) - (pixel_width/2);
+            x_right = xdata(2) + (pixel_width/2);
+            x = linspace(x_left, x_right, N+1);
+            
+            self.xv = zeros(1, 2*numel(x));
+            self.xv(1:2:end) = x;
+            self.xv(2:2:end) = x;
+            
+            self.yv = repmat([y(1) ; y(end)], 1, numel(x));
+            self.yv(:,2:2:end) = flipud(self.yv(:,2:2:end));
+            
+            self.xv = self.xv(:);
+            self.yv = self.yv(:);
+            
+            self.yh = zeros(1, 2*numel(y));
+            self.yh(1:2:end) = y;
+            self.yh(2:2:end) = y;
+            
+            self.xh = repmat([x(1) ; x(end)], 1, numel(y));
+            self.xh(:,2:2:end) = flipud(self.xh(:,2:2:end));
+            
+            self.xh = self.xh(:);
+            self.yh = self.yh(:);
+        end
         
-      else
-        pixel_height = 1;
-      end
-      
-      if N > 1
-        pixel_width = diff(xdata) / (N-1);
-      else
-        pixel_width = 1;
-      end
-      
-      y_top = ydata(1) - (pixel_height/2);
-      y_bottom = ydata(2) + (pixel_height/2);
-      y = linspace(y_top, y_bottom, M+1);
-      
-      x_left = xdata(1) - (pixel_width/2);
-      x_right = xdata(2) + (pixel_width/2);
-      x = linspace(x_left, x_right, N+1);
-      
-      self.xv = zeros(1, 2*numel(x));
-      self.xv(1:2:end) = x;
-      self.xv(2:2:end) = x;
-      
-      self.yv = repmat([y(1) ; y(end)], 1, numel(x));
-      self.yv(:,2:2:end) = flipud(self.yv(:,2:2:end));
-      
-      self.xv = self.xv(:);
-      self.yv = self.yv(:);
-      
-      self.yh = zeros(1, 2*numel(y));
-      self.yh(1:2:end) = y;
-      self.yh(2:2:end) = y;
-      
-      self.xh = repmat([x(1) ; x(end)], 1, numel(y));
-      self.xh(:,2:2:end) = flipud(self.xh(:,2:2:end));
-      
-      self.xh = self.xh(:);
-      self.yh = self.yh(:);
+        function displayGrid(self)
+            % displays grid
+            line('Parent', self.view, 'XData', self.xh, 'YData', self.yh, ...
+                'Color', 'k', 'Clipping', 'off');
+            line('Parent', self.view, 'XData', self.xv, 'YData', self.yv, ...
+                'Color', 'k', 'Clipping', 'off');
+        end
+        
+        function displayText(self)
+            textStrings = num2str(self.val(:));
+            textStrings = strtrim(cellstr(textStrings));
+            [x, y] = meshgrid(1:8, 1:192/8);
+            self.hStrings = text(self.view, x(:),y(:), textStrings(:),...
+                'HorizontalAlignment', 'center');
+            set(self.hStrings, 'ButtonDownFcn', @self.selectorCallback);
+        end
+        
+        function [column, row] = selection(~, selected)
+            % return row and column of selected channel
+            remainder = mod(selected, 8);
+            row = floor(selected/8) + 1;
+            if remainder == 0
+                column = 8;
+                row = row - 1;
+            else
+                column = remainder;
+            end
+        end
+        
+        function update(self)
+            % update view to current selected
+            imagesc(self.view, self.mat);
+            self.displayText;
+            self.displayGrid;
+            
+            set(self.view,'XTick',[], 'XTickLabel',[],...
+                'YTick',[], 'YTickLabel',[], 'TickLength', [0 0]);
+        end
+        
+        function selectorCallback(self, src, ~)
+            % callback function for selector
+            [column, row] = self.selection(self.selected);
+            self.mat(row, column) = 0;
+            self.selected = str2double(src.String);
+            [column, row] = self.selection(self.selected);
+            self.mat(row, column) = 1;
+            
+            self.update;
+            
+            self.callback(self.selected);
+        end
     end
     
-    function displayGrid(self)
-      % displays grid
-      line('Parent', self.view, 'XData', self.xh, 'YData', self.yh, ...
-        'Color', 'k', 'Clipping', 'off');
-      line('Parent', self.view, 'XData', self.xv, 'YData', self.yv, ...
-        'Color', 'k', 'Clipping', 'off');
-    end
-    
-    function displayText(self)
-      textStrings = num2str(self.val(:));
-      textStrings = strtrim(cellstr(textStrings));
-      [x, y] = meshgrid(1:8, 1:192/8);
-      self.hStrings = text(self.view, x(:),y(:), textStrings(:),...
-        'HorizontalAlignment', 'center');
-      set(self.hStrings, 'ButtonDownFcn', @self.selectorCallback);
-    end
-    
-    function [column, row] = selection(~, selected)
-      % return row and column of selected channel
-      remainder = mod(selected, 8);
-      row = floor(selected/8) + 1;
-      if remainder == 0
-        column = 8;
-        row = row - 1;
-      else
-        column = remainder;
-      end 
-    end
-  
-    function update(self)
-      % update view to current selected
-      imagesc(self.view, self.mat);
-      self.displayText;
-      self.displayGrid;
-      
-      set(self.view,'XTick',[], 'XTickLabel',[],...
-        'YTick',[], 'YTickLabel',[], 'TickLength', [0 0]);
-    end
-    
-    function selectorCallback(self, src, ~)
-      % callback function for selector
-      [column, row] = self.selection(self.selected);
-      self.mat(row, column) = 0;
-      self.selected = str2double(src.String);
-      [column, row] = self.selection(self.selected);
-      self.mat(row, column) = 1;
-      
-      self.update;
-      
-      self.callback(self.selected);
-    end
-  end
-
 end
